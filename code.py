f0da91bcf6a17335131a436fd5e9ea25230b27c5
agniteja23 <81700677+agniteja23@users.noreply.github.com>
2021-06-20 13:08:04 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,18 @@
+#importing Required libraries
+import matplotlib.pyplot as plt
+import cv2
+import easyocr
+from pylab import rcParams
+from IPython.display import Image
+rcParams['figure.figsize'] = 8, 16
+
+#downloading the reader module from easyocr.
+reader = easyocr.Reader(['en'])
+
+#loading the image.
+Image("Capture.jpg")
+#reading the Characters in that image.
+output = reader.readtext('Capture.jpg',detail=0,paragraph=True)
+print(output)
+output
+
